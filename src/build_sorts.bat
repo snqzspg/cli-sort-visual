0aec954949d4f02e4418300032a6cfb5b8c562e4
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-07-22 03:33:20 +0800

    Updated build version

--- src/build_sorts.bat
+++ src/build_sorts.bat
@@ -11,7 +11,7 @@ echo.
 
 for /f "usebackq delims=" %%i in (build_list.txt) do (
     echo Building %%i.c
-    "%gcc_cmd%" -Wall -O2 -DDEFAULT_DIS_DELAY=10 -DACCOUNT_PRINT_LEN -DPRINT_TIME_W_O_DELAY=0 -DVERSION="""1.1""" -DBUILD="""202207150139""" -DIS_SNAPSHOT=1 terminal_visualiser.c helper/precise_and_acc_time.c helper/beep_log.c helper/anti_quicksort.c helper/ioext.c helper/triangular_input.c "%%i.c" -o "..\%%i"
+    "%gcc_cmd%" -Wall -O2 -DDEFAULT_DIS_DELAY=10 -DACCOUNT_PRINT_LEN -DPRINT_TIME_W_O_DELAY=0 -DVERSION="""1.1""" -DBUILD="""202207180145""" -DIS_SNAPSHOT=1 terminal_visualiser.c helper/precise_and_acc_time.c helper/beep_log.c helper/anti_quicksort.c helper/ioext.c helper/triangular_input.c "%%i.c" -o "..\%%i"
     @REM "%gcc_cmd%" -Wall -O2 terminal_visualiser.c "%%i.c" -o "..\%%i"
 )
 mkdir ..\sounds
