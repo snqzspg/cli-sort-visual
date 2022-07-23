3e7d9ba9b23170f5e678d5ea3d8b4742ecf8e0fc
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-07-23 16:39:01 +0800

    Updated build number

--- src/build_sorts_ncurses.bash
+++ src/build_sorts_ncurses.bash
@@ -10,7 +10,7 @@ gcc_cmd=gcc
 while read i; do
 	echo Building $i.c;
 	# "$gcc_cmd" -Wall -O2 -DUSE_ANSI_CLEAR terminal_visualiser.c "$i.c" -o "../$i" -lm;
-	"$gcc_cmd" -Wall -O2 -DVERSION=\"1.1\" -DBUILD=\"202207200235\" -DIS_SNAPSHOT=1 terminal_visualiser.c helper/precise_and_acc_time.c helper/beep_log.c helper/ioext.c helper/triangular_input.c helper/smooth_heapify.c "$i.c" -o "../$i" -lm -lcurses;
+	"$gcc_cmd" -Wall -O2 -DVERSION=\"1.1\" -DBUILD=\"202207231608\" -DIS_SNAPSHOT=1 terminal_visualiser.c helper/precise_and_acc_time.c helper/beep_log.c helper/ioext.c helper/triangular_input.c helper/smooth_heapify.c "$i.c" -o "../$i" -lm -lcurses;
 done < "build_list.txt";
 mkdir -p ../sounds;
 "$gcc_cmd" -Wall -O2 make_sound.c -o ../sounds/make_wav -lm;
